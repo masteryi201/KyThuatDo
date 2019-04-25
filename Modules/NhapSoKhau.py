e15a1fc7c1b716365323f0ec5d198dbfb4fa1707
masteryi201 <mg3gold2014@gmail.com>
2019-04-25 02:37:09 -0700

    Update NhapSoKhau.py

--- Modules/NhapSoKhau.py
+++ Modules/NhapSoKhau.py
@@ -1,7 +1,10 @@
 import os
 import sys
 from pathlib import Path
+from subprocess import call
+
 mypath = Path().absolute()
+path_creScript1 = os.path.join(mypath, "..\\Lib\\creScript1.py")
 join_path = os.path.join(mypath, "..\\Temp")
 path_config = os.path.join(mypath, "..\\Temp\\config.py")
 sys.path.append(join_path)  
@@ -10,4 +13,5 @@ from config import *
 	# Nhập số khâu tăng và giảm
 input_value_SizeUp = SoKhauTang
 input_value_SizeDown = SoKhauGiam
+call(["python", path_creScript1])
 
